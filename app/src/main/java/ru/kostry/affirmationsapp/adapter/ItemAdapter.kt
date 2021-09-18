7e20c95a365259b6e14aea0d281ae0bab389811f
konstantin <kosval09@gmail.com>
2021-09-18 13:56:29 +0900

    добалены картинки

--- app/src/main/java/ru/kostry/affirmationsapp/adapter/ItemAdapter.kt
+++ app/src/main/java/ru/kostry/affirmationsapp/adapter/ItemAdapter.kt
@@ -4,16 +4,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ru.kostry.affirmationsapp.R
 import ru.kostry.affirmationsapp.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>)
-    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -24,7 +26,8 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text =  context.resources.getString(item.stringResourceId)
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
